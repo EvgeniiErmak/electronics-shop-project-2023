019d67972b7e8b28ae18c54cc24f79cee1541723
Evgenii Ermak <djermak3000@mail.ru>
2023-10-04 23:11:25 +0300

    homework-1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,4 +1,23 @@
-from src.item import Item
+class Item:
+    # Атрибут класса для хранения уровня цен с учетом скидки
+    pay_rate = 1.0
+
+    # Атрибут класса для хранения созданных экземпляров класса
+    all_items = []
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all_items.append(self)
+
+    def calculate_total_price(self):
+        # Метод для вычисления общей стоимости товара с учетом количества
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        # Метод для применения скидки к товару
+        self.price *= self.pay_rate
 
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
@@ -7,12 +26,12 @@ if __name__ == '__main__':
     print(item1.calculate_total_price())  # 200000
     print(item2.calculate_total_price())  # 100000
 
-    # устанавливаем новый уровень цен
+    # Устанавливаем новый уровень цен
     Item.pay_rate = 0.8
-    # применяем скидку
+    # Применяем скидку
     item1.apply_discount()
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
 
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    print(Item.all_items)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item('Товар', 100, 10)
+
+
+def test_init(item):
+    assert item.name == 'Товар'
+    assert item.price == 100
+    assert item.quantity == 10
+    assert len(Item.all) == 1
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 1000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.9
+    item.apply_discount()
+    assert item.price == 90
