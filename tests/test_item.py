e324d8210c72a871245f4916b9b24a962455d106
Evgenii Ermak <djermak3000@mail.ru>
2023-10-08 19:52:47 +0300

    homework 3 update tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,60 +1,135 @@
 import pytest
+import csv
 from src.item import Item
 
 
+# Подготовительная работа для тестирования
+@pytest.fixture
+def create_test_items(tmp_path):
+    """
+    Фикстура для создания тестовых данных и экземпляров класса Item.
+    """
+    test_data = [
+        {'name': 'Test1', 'price': '10', 'quantity': '2'},
+        {'name': 'Test2', 'price': '20', 'quantity': '3'},
+    ]
+    test_file = tmp_path / 'test_items.csv'
+    with open(test_file, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['name', 'price', 'quantity'])
+        writer.writeheader()
+        writer.writerows(test_data)
+
+    Item.instantiate_from_csv(str(test_file))
+    yield test_file
+    Item.all = []
+
 # Тесты для класса Item
-class TestItem:
+def test_instantiate_from_csv(create_test_items):
+    """
+    Тестирование метода instantiate_from_csv класса Item.
+    """
+    assert len(Item.all) == 2
+    assert Item.all[0].name == 'Test1'
+    assert Item.all[1].name == 'Test2'
 
-    @pytest.fixture
-    def sample_item(self):
-        # Фикстура для создания примера товара
-        return Item("Sample Item", 10.0, 5)
+def test_string_to_number():
+    """
+    Тестирование метода string_to_number класса Item.
+    """
+    assert Item.string_to_number('42') == 42
 
-    def test_init(self, sample_item):
-        """
-        Проверяем правильность инициализации объекта Item.
-        """
-        assert sample_item.name == "Sample Item"
-        assert sample_item.price == 10.0
-        assert sample_item.quantity == 5
+def test_name_property():
+    """
+    Тестирование свойства name класса Item.
+    """
+    item = Item('Test', 10, 1)
+    assert item.name == 'Test'
+    item.name = 'VeryLongNameForTest'
+    assert item.name == 'VeryLongNa'  # Изменено на 'VeryLongNa'
 
-    def test_name_property(self, sample_item):
-        """
-        Проверяем, что свойство name работает правильно.
-        """
-        sample_item.name = "A very long name for an item"
-        assert sample_item.name == "A very lon"
+def test_calculate_total_price():
+    """
+    Тестирование метода calculate_total_price класса Item.
+    """
+    item = Item('Test', 10, 3)
+    assert item.calculate_total_price() == 30
 
-    def test_calculate_total_price(self, sample_item):
-        """
-        Проверяем правильность расчета общей стоимости товара.
-        """
-        assert sample_item.calculate_total_price() == 50.0  # 10.0 * 5
+def test_apply_discount():
+    """
+    Тестирование метода apply_discount класса Item.
+    """
+    item = Item('Test', 10, 1)
+    item.pay_rate = 0.9
+    item.apply_discount()
+    assert item.price == 9.0
 
-    def test_apply_discount(self, sample_item):
-        """
-        Проверяем применение скидки к товару.
-        """
-        sample_item.pay_rate = 0.9  # 10% скидка
-        sample_item.apply_discount()
-        assert sample_item.price == 9.0  # 10.0 * 0.9
+def test_name_property_setter_short_name():
+    """
+    Тестирование сеттера свойства name класса Item для короткого имени.
+    """
+    item = Item('Test', 10, 1)
+    item.name = 'Short'
+    assert item.name == 'Short'
 
-    def test_instantiate_from_csv(self, tmp_path):
-        """
-        Проверяем инициализацию товаров из CSV-файла.
-        """
-        # Создаем временный CSV-файл
-        csv_data = "name,price,quantity\nItem1,5.0,10\nItem2,8.0,20"
-        csv_file = tmp_path / "test_item.csv"
-        csv_file.write_text(csv_data)
+def test_name_property_setter_long_name():
+    """
+    Тестирование сеттера свойства name класса Item для длинного имени.
+    """
+    item = Item('Test', 10, 1)
+    item.name = 'VeryLongNameForTest'
+    assert item.name == 'VeryLongNa'  # Изменено на 'VeryLongNa'
 
-        # Инициализируем товары из CSV
-        Item.instantiate_from_csv(str(csv_file))
+def test_calculate_total_price_zero_quantity():
+    """
+    Тестирование метода calculate_total_price класса Item с нулевым количеством товара.
+    """
+    item = Item('Test', 10, 0)
+    assert item.calculate_total_price() == 0
 
-        assert len(Item.all) == 2
-        assert Item.all[0].name == "Item1"
-        assert Item.all[0].price == 5.0
-        assert Item.all[0].quantity == 10
-        assert Item.all[1].name == "Item2"
-        assert Item.all[1].price == 8.0
-        assert Item.all[1].quantity == 20
+def test_calculate_total_price_negative_price():
+    """
+    Тестирование метода calculate_total_price класса Item с отрицательной ценой товара.
+    """
+    item = Item('Test', -10, 3)
+    assert item.calculate_total_price() == -30
+
+def test_apply_discount_no_discount():
+    """
+    Тестирование метода apply_discount класса Item без скидки.
+    """
+    item = Item('Test', 10, 1)
+    item.pay_rate = 1.0
+    item.apply_discount()
+    assert item.price == 10.0
+
+def test_apply_discount_large_discount():
+    """
+    Тестирование метода apply_discount класса Item с большой скидкой.
+    """
+    item = Item('Test', 10, 1)
+    item.pay_rate = 0.1
+    item.apply_discount()
+    assert item.price == 1.0
+
+def test_init_method():
+    """
+    Тестирование конструктора __init__ класса Item.
+    """
+    item = Item('Test', 10, 1)
+    assert item.name == 'Test'
+    assert item.price == 10
+    assert item.quantity == 1
+
+def test_str_method():
+    """
+    Тестирование метода __str__ класса Item.
+    """
+    item = Item('Test', 10, 1)
+    assert str(item) == 'Test'
+
+def test_repr_method():
+    """
+    Тестирование метода __repr__ класса Item.
+    """
+    item = Item('Test', 10, 1)
+    assert repr(item) == "Item('Test', 10, 1)"
