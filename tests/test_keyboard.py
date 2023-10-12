a2f889a9832d4525e1f3fe500a6bbcff8ed23f4c
Evgenii Ermak <djermak3000@mail.ru>
2023-10-13 01:57:05 +0300

    homework 5 done - update test_keyboard.py

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,36 +1,58 @@
 import pytest
-from src.keyboard import Keyboard
+from src.keyboard import KeyboardLayoutMixin, Keyboard
 
-# Тестирование класса Keyboard
+def test_keyboard_layout_mixin_default_layout():
+    """
+    Проверка раскладки клавиатуры по умолчанию.
+    """
+    mixin = KeyboardLayoutMixin()
+    assert mixin.layout == "EN"
 
-def test_keyboard_init():
-    kb = Keyboard("Dark Project", "KD87A", 50, "EN")
-    assert kb.name == "Dark Project"
-    assert kb.model == "KD87A"
-    assert kb.price == 50
-    assert kb.language == "EN"
-    assert kb.layout == "EN"
+def test_keyboard_layout_mixin_change_layout_valid_option():
+    """
+    Проверка изменения раскладки клавиатуры на допустимую опцию.
+    """
+    mixin = KeyboardLayoutMixin()
+    mixin.change_layout("RU")
+    assert mixin.layout == "RU"
 
-def test_keyboard_change_lang():
-    kb = Keyboard("Dark Project", "KD87A", 50, "EN")
-    kb.change_lang("RU")
-    assert kb.language == "RU"
+def test_keyboard_layout_mixin_change_layout_invalid_option():
+    """
+    Проверка изменения раскладки клавиатуры на недопустимую опцию.
+    """
+    mixin = KeyboardLayoutMixin()
+    mixin.change_layout("CH")
+    assert mixin.layout == "EN"
 
-    kb.change_lang("EN")
-    assert kb.language == "EN"
+def test_keyboard_default_language():
+    """
+    Проверка языка клавиатуры по умолчанию.
+    """
+    keyboard = Keyboard("Название", "Модель", 50)
+    assert keyboard.language == "EN"
 
-def test_keyboard_change_layout():
-    kb = Keyboard("Dark Project", "KD87A", 50, "EN")
-    kb.change_layout("RU")
-    assert kb.layout == "RU"
+def test_keyboard_valid_language_option():
+    """
+    Проверка изменения языка клавиатуры на допустимую опцию.
+    """
+    keyboard = Keyboard("Название", "Модель", 50)
+    keyboard.change_lang("RU")
+    assert keyboard.language == "RU"
 
-    kb.change_layout("EN")
-    assert kb.layout == "EN"
+def test_keyboard_invalid_language_option():
+    """
+    Проверка изменения языка клавиатуры на недопустимую опцию.
+    """
+    keyboard = Keyboard("Название", "Модель", 50)
+    keyboard.change_lang("CH")
+    assert keyboard.language == "EN"
 
-def test_keyboard_str():
-    kb = Keyboard("Dark Project", "KD87A", 50, "EN")
-    assert str(kb) == "Dark Project KD87A"
+def test_keyboard_string_representation():
+    """
+    Проверка строкового представления клавиатуры.
+    """
+    keyboard = Keyboard("Название", "Модель", 50)
+    assert str(keyboard) == "Название Модель"
 
-# Запуск тестов
 if __name__ == '__main__':
     pytest.main()
