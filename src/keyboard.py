7ea311db868eb0b34e1c55e6871924485136eae6
Evgenii Ermak <djermak3000@mail.ru>
2023-10-13 01:47:05 +0300

    homework 5 done - update keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,42 +1,24 @@
-# Класс-миксин для хранения и изменения раскладки клавиатуры
 class KeyboardLayoutMixin:
     def __init__(self):
-        self.layout = "EN"  # Изначально устанавливаем раскладку на английскую
+        self.layout = "EN"
 
     def change_layout(self, new_layout):
-        if new_layout in ["EN", "RU"]:
+        layout_options = ["EN", "RU"]
+        if new_layout in layout_options:
             self.layout = new_layout
-            print(f"Раскладка клавиатуры изменена на {new_layout}")
-        else:
-            print("Неподдерживаемая раскладка. Доступны только 'EN' и 'RU'.")
 
-
-# Основной класс Keyboard для товара "клавиатура"
 class Keyboard(KeyboardLayoutMixin):
     def __init__(self, name, model, price, language="EN"):
-        super().__init__()  # Вызываем конструктор миксина
+        super().__init__()
         self.name = name
         self.model = model
         self.price = price
-        self.language = language
+        self.language = language if language in ["EN", "RU"] else "EN"
 
     def change_lang(self, new_language):
-        if new_language in ["EN", "RU"]:
+        language_options = ["EN", "RU"]
+        if new_language in language_options:
             self.language = new_language
-            print(f"Язык клавиатуры изменен на {new_language}")
-        else:
-            print("Неподдерживаемый язык. Доступны только 'EN' и 'RU'.")
 
     def __str__(self):
         return f"{self.name} {self.model}"
-
-
-# Пример использования
-if __name__ == "__main__":
-    kb = Keyboard("Dark Project", "KD87A", 50, "EN")
-    print(f"Товар: {kb}, Цена: {kb.price}, Язык: {kb.language}, Раскладка: {kb.layout}")
-
-    kb.change_lang("RU")
-    kb.change_layout("RU")
-
-    print(f"Товар: {kb}, Цена: {kb.price}, Язык: {kb.language}, Раскладка: {kb.layout}")
